b13397a802d948dcc8b3fd7c47afa9f09a7c8215
Jeremy Steele <jeremy@jeremysteele.net>
2024-12-04 22:00:47 -0500

    Refactor live chat

--- 4/main.go
+++ 4/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
@@ -27,74 +26,4 @@ func main() {
 	part1Search(wordSearch)
 
 	part2Search(wordSearch)
-}
-
-func part2Search(wordSearch [][]rune) {
-	tally := 0
-
-	for y, yChars := range wordSearch {
-		for x, xChar := range yChars {
-			if xChar == 'A' {
-				//Check constraints
-				if x-1 < 0 {
-					continue
-				} else if x+1 >= len(wordSearch[y]) {
-					continue
-				} else if y-1 < 0 {
-					continue
-				} else if y+1 >= len(wordSearch[y]) {
-					continue
-				}
-
-				posTally := 0
-				posTally += part2SearchLeft(wordSearch, x, y)
-				posTally += part2SearchRight(wordSearch, x, y)
-
-				if posTally != 2 {
-					continue
-				}
-
-				fmt.Printf("%d, %d: %d\n", x, y, posTally)
-				tally++
-			}
-		}
-	}
-
-	fmt.Printf("Total: %d\n", tally)
-}
-
-func part2SearchLeft(wordSearch [][]rune, x int, y int) int {
-	tally := 0
-
-	if wordSearch[y-1][x-1] == 'M' {
-		if wordSearch[y+1][x+1] == 'S' {
-			tally++
-		}
-	}
-
-	if wordSearch[y+1][x-1] == 'M' {
-		if wordSearch[y-1][x+1] == 'S' {
-			tally++
-		}
-	}
-
-	return tally
-}
-
-func part2SearchRight(wordSearch [][]rune, x int, y int) int {
-	tally := 0
-
-	if wordSearch[y-1][x+1] == 'M' {
-		if wordSearch[y+1][x-1] == 'S' {
-			tally++
-		}
-	}
-
-	if wordSearch[y+1][x+1] == 'M' {
-		if wordSearch[y-1][x-1] == 'S' {
-			tally++
-		}
-	}
-
-	return tally
 }
